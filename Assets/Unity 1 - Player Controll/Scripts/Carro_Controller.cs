886f83ab82673f1b71b5a589061650a3b2e21a0a
AlexandreBS <sousabalexandre@gmail.com>
2024-02-29 22:11:27 +0000

    Lesson_1_1.4
    Acabei o exercício do carro.

--- Assets/Unity 1 - Player Controll/Scripts/Carro_Controller.cs
+++ Assets/Unity 1 - Player Controll/Scripts/Carro_Controller.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Carro_Controller : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private float spd = 20f;
+    private float turnspd = 45f;
+    private float horizontalInput;
+    private float forwardInput;
     void Start()
     {
         
@@ -14,7 +19,11 @@ public class Carro_Controller : MonoBehaviour
     void Update()
     {
 
-
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //caro anda para a frente
+        transform.Translate(Vector3.forward * Time.deltaTime * spd);
+        //carro roda a saia
+        transform.Rotate(Vector3.up, turnspd * Time.deltaTime * horizontalInput);
     }
 }
